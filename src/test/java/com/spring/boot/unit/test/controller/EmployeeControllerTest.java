f5ebe119e7b7e3e46c6468eec6e03761cc7fea37
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-17 14:53:49 +0700

    add employee controller test

---
+++ src/test/java/com/spring/boot/unit/test/controller/EmployeeControllerTest.java
@@ -0,0 +1,40 @@
+package com.spring.boot.unit.test.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+
+public class EmployeeControllerTest {
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setup(){
+		this.mockMvc = MockMvcBuilders
+	            .standaloneSetup(new EmployeeController())
+	            .build();
+	}
+	
+	@Test
+	public void getAllEmployees() throws Exception {
+		this.mockMvc.perform(post("/employees")
+				.content("{\"name\":\"andy\",\"gender\":\"shemale\"}")
+				.contentType("application/json"))
+		.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void getMaleEmployees() throws Exception {
+		String result = "[{\"name\":\"ali\",\"gender\":\"male\"}]";
+		
+		this.mockMvc.perform(get("/employees?gender=male"))
+		.andExpect(content().json(result));
+	}
+
+}
