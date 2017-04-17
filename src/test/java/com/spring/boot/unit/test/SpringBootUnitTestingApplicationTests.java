b1f7dcefe280e988b110df08ac5710982945ddfe
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-17 12:50:52 +0700

    Initial commit

---
+++ src/test/java/com/spring/boot/unit/test/SpringBootUnitTestingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.boot.unit.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootUnitTestingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
